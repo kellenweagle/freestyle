f38b98a98545103bd772c94696e217a7ce578257
Kellen <kellenweagle@gmail.com>
2024-12-21 14:07:08 -0800

    push

--- backend/db/migrations/03-product-images.js
+++ backend/db/migrations/03-product-images.js
@@ -7,7 +7,6 @@ if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;
 }
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable(options, {
