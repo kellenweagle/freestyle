7e4e3e4230af090421ef80d1ceb61b00d1d76f05
Kellen <kellenweagle@gmail.com>
2024-12-21 13:32:44 -0800

    pushing

--- backend/config/database.js
+++ backend/config/database.js
@@ -19,10 +19,6 @@ module.exports = {
         seederStorage: 'sequelize'
     },
     production: {
-        username,
-        password,
-        database,
-        host,
         use_env_variable: 'DATABASE_URL',
         dialect: 'postgres',
         seederStorage: 'sequelize',
